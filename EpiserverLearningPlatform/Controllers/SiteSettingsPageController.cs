236b3dbe67b83baab5f6518e81af166c25695506
Andrew Durnev <almatinez101@yandex.ru>
2021-06-23 02:03:05 +0300

    Introduced SiteSettings Page.

---
+++ EpiserverLearningPlatform/Controllers/SiteSettingsPageController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+using EpiserverLearningPlatform.Models.Pages;
+using EpiserverLearningPlatform.Models.ViewModels;
+
+namespace EpiserverLearningPlatform.Controllers
+{
+    public class SiteSettingsPageController : PageControllerBase<SiteSettingsPage>
+    {
+        public ActionResult Index(SiteSettingsPage currentPage)
+        {
+            var model = PageViewModel.Create(currentPage);
+            return View(model);
+        }
+    }
+}
---
+++ EpiserverLearningPlatform/Models/Pages/SiteSettingsPage.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using EPiServer.Web;
+using StructureMap;
+
+namespace EpiserverLearningPlatform.Models.Pages
+{
+    [SiteContentType(
+        GUID = "F808522C-0AF9-4815-B532-DAD16DE0235B",
+        GroupName = Global.GroupNames.SiteSettings)]
+    public class SiteSettingsPage : SitePageData
+    {
+        [Display(GroupName = Global.GroupNames.SiteSettings, Order = 10)]
+        public virtual bool SomeFeatureEnabled { get; set; }
+    }
+}
