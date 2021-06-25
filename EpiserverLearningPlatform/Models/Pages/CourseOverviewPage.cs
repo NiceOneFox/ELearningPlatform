f132a1a342f0f8b3d66efd5e713eff6904d4ea85
NiceOneFox <kulakovskij.es@edu.spbstu.ru>
2021-06-25 20:17:20 +0300

    Added Models For Overview course page

---
+++ EpiserverLearningPlatform/Models/Pages/CourseOverviewPage.cs
@@ -0,0 +1,25 @@
+﻿using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.SpecializedProperties;
+using EpiserverLearningPlatform.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace EpiserverLearningPlatform.Models.Pages
+{
+    [ContentType(DisplayName = "CourseOverviewPage", GUID = "f13603ae-a7ec-4ac2-bd7d-1ae64a613553", Description = "")]
+    public class CourseOverviewPage : IPageViewModel<SitePageData>
+    {
+        [Display(
+        GroupName = SystemTabNames.Content,
+        Order = 10)]
+        public virtual List<CourseModel> Courses { get; set; }
+
+        public SitePageData CurrentPage => throw new NotImplementedException();
+
+        public LayoutModel Layout { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public IContent Section { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
---
+++ EpiserverLearningPlatform/Models/ViewModels/CourseModel.cs
@@ -0,0 +1,19 @@
+﻿using EPiServer.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EpiserverLearningPlatform.Models.ViewModels
+{
+    public class CourseModel
+    {
+        public ContentReference Image { get; set; }
+
+        public string CourseName { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+    }
+}
