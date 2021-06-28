90ff7b4290b367d2a4c9e52bf932619d6e7ab218
YuliaKUA <mon.ev13@mail.ru>
2021-06-28 17:04:38 +0300

    #130 update course page

--- EpiserverLearningPlatform/Models/Pages/CoursePage.cs
+++ EpiserverLearningPlatform/Models/Pages/CoursePage.cs
@@ -1,7 +1,12 @@
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.SpecializedProperties;
 using System;
 using System.ComponentModel.DataAnnotations;
 using EPiServer.Web;
 using EPiServer.Core;
+using EPiServer;
 
 namespace EpiserverLearningPlatform.Models.Pages
 {
@@ -26,5 +31,19 @@ namespace EpiserverLearningPlatform.Models.Pages
         
         [Display(GroupName = Global.GroupNames.Contact, Order = 50)]
         public virtual DateTime EndDate { get; set; }
+
+        [Display(GroupName = Global.GroupNames.Contact, Order = 60)]
+        [UIHint(UIHint.Image)]
+        public virtual ContentReference CalendarImage { get; set; }
+
+
+        
+        [Display(Order = 1, GroupName = Global.GroupNames.Contact)]
+        [Required]
+        public virtual string ButtonText { get; set; }
+
+        [Display(Order = 2, GroupName = Global.GroupNames.Contact)]
+        [Required]
+        public virtual Url ButtonLink { get; set; }
     }
 }
